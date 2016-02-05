ba493f24531436e3228f9907d3dd1871395f5a73
Shamir Ajate <shamirajate@gmail.com>
2016-02-04 21:10:48 -0500

    Changed Form Heading, added E-mail Glyphicon

--- Animation.js
+++ Animation.js
@@ -1,7 +1,7 @@
 // JavaScript source code
 $(document).ready(function () {
     $('#Logo').hide().fadeIn(1000);
-    $('#WebDesigns').hide().fadeIn(6000);
+    $('#WebDesigns').hide().fadeIn(4000);
     $('#LogoCreationBanner').hide().slideDown(500);
     $('#WebDesignBanner').hide().slideDown(500);
     $('#UIDesignBanner').hide().slideDown(500);
